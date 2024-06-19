9099d688e5927dddad4fb0b45599a2ad39b8c9b0
MarcosFGimenes <142637215+MarcosFGimenes@users.noreply.github.com>
2024-06-19 19:38:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 
-for(let i = 0; i < botoes.length; i++){
-    botoes[i].onclick = function() {
-        // Primeiro, remova a classe 'ativo' de todos os botões
-        for(let j = 0; j < botoes.length; j++){
-            botoes[j].classList.remove('ativo');
-            botoes[j].style.backgroundColor = ""; // Redefina a cor de fundo para vazio
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
         }
-        // Em seguida, adicione a classe 'ativo' ao botão clicado
-        this.classList.add('ativo'); // Usamos 'this' para se referir ao botão atual
-        this.style.backgroundColor = "#FF5733"; // Substitua pela cor desejada
+
+        botoes[i].classList.add("ativo");
     }
 }
