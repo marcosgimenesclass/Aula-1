575b63253fbed59361af574cc015cb82e2410876
MarcosFGimenes <142637215+MarcosFGimenes@users.noreply.github.com>
2024-07-03 19:45:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function() {
 
-        for(let j=0;j<botoes.length;j++){
+        // Remover a classe 'ativo' de todos os botões
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
         }
 
+        // Adicionar a classe 'ativo' apenas ao botão clicado
         botoes[i].classList.add("ativo");
+
+        // Mostrar a aba correspondente
+        for (let k = 0; k < abas.length; k++) {
+            abas[k].classList.remove("ativo");
+        }
+        abas[i].classList.add("ativo");
     }
 }
